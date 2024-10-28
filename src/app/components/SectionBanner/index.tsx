1d904dd4ae4c464814d6daf77d16f8a58be73e5b
RobertoDeOliveira <robertomelo287@gmail.com>
2024-10-28 01:05:56 -0300

    config-begin: add past of components and begin config of SectionBanner

---
+++ src/app/components/SectionBanner/index.tsx
@@ -0,0 +1,7 @@
+export const SectionBanner = () => {
+  return (
+    <div className="grid grid-cols-subgrid col-span-full">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { SectionBanner } from "./components/SectionBanner";
+
 export default function Home() {
   return (
     <main className="bg-red-700 max-w-[1440px] m-auto grid grid-cols-11">
-      <div>hello world</div>
+      <SectionBanner />
     </main>
   );
 }
